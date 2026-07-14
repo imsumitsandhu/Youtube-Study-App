6e91ea06250206a61cf7dbe6df162a183e70f99b
Sumit Sandhu <imsumitsandhu@Mac.lan>
2026-07-15 02:01:26 +0530

    Small Update in Prompt

--- services/geminiservice.js
+++ services/geminiservice.js
@@ -2,7 +2,7 @@ const ai = require("../config/gemini");
 
 async function generateSummary(transcript) {
     const prompt = `
-You are an expert teacher.
+You are an expert.
 
 Summarize the following YouTube transcript in simple language.
 Also give important points to keep in mind.
